06400af871de24a209b75e8145a3e851de2b08a3
fztfztfzt <fztfztfzt@qq.com>
2016-02-17 22:17:14 +0800

    2016-2-17

--- medium/334 Increasing Triplet Subsequence.cpp
+++ medium/334 Increasing Triplet Subsequence.cpp
@@ -40,3 +40,18 @@ public:
         return false;
     }
 };
+Runtime: 8 ms
+bool increasingTriplet(vector<int>& nums) {
+       int min1=INT_MAX,min2=INT_MAX;
+       for(int i=0;i<nums.size();++i)
+       {
+          if(nums[i]<=min1)
+          {
+            min1 = nums[i];
+          }else if(nums[i]<=min2)
+          {
+              min2 = nums[i];
+          }else return true;
+       }
+       return false;
+    }
